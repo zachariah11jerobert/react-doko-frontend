3c55333ec7eb9a0f90c40d97d04eec7e6244b5c4
unknown <zachariah11jerobert@gmail.com>
2022-02-05 02:31:59 +0530

    done bug fix

--- src/components/HeroRibbon/HeroRibbon.js
+++ src/components/HeroRibbon/HeroRibbon.js
@@ -1,74 +1,41 @@
 import React from "react";
 import "./heroRibbon.css";
 
-import review from '../../assets/4.8new.png';
-import img1 from '../../assets/mailicon3.png';
-import img2 from '../../assets/mailicon1.png';
-import img3 from '../../assets/mailicon2.png';
+import review from "../../assets/4.8new.png";
+import img1 from "../../assets/mailicon3.png";
+import img2 from "../../assets/mailicon1.png";
+import img3 from "../../assets/mailicon2.png";
 
 const HeroRibbon = () => {
   return (
     <section style={{ backgroundColor: "#1c2c46" }}>
-      <div className="container_ribbon">
-        <div className="flex-1of4">
-          <img
-            src={review}
-            style={{ width: 250 }}
-          />
+      <div className="container_main">
+        <div className="flex-right">
+          <img src={review} style={{ width: 250 }} />
         </div>
 
-        <div className="flex-3of4 container_ribbon">
+        <div className="flex-left container_ribbon">
           <div className="box_ribbon">
-            <img
-              src={img1}
-              alt="blog"
-            />
+            <img src={img1} alt="blog" />
             <div className="margin-left-10">
               <h4 style={{ color: "#fff" }}>No Monthly</h4>
-              <p
-                style={{
-                  color: "#ffffffad",
-                  fontSize: "17px",
-                }}
-              >
-                Commitment.
-              </p>
+              <p>Commitment.</p>
             </div>
           </div>
 
           <div className="box_ribbon">
-            <img
-              src={img2}
-              alt="blog"
-            />
+            <img src={img2} alt="blog" />
             <div className="margin-left-10">
               <h4 style={{ color: "#fff" }}>FREE Doctor</h4>
-              <p
-                style={{
-                  color: "#ffffffad",
-                  fontSize: "17px",
-                }}
-              >
-                Consultations
-              </p>
+              <p>Consultations</p>
             </div>
           </div>
 
           <div className="box_ribbon">
-            <img
-              src={img3}
-              alt="blog"
-            />
+            <img src={img3} alt="blog" />
             <div className="margin-left-10">
               <h4 style={{ color: "#fff" }}>No Membership</h4>
-              <p
-                style={{
-                  color: "#ffffffad",
-                  fontSize: "17px",
-                }}
-              >
-                Fees
-              </p>
+              <p>Fees</p>
             </div>
           </div>
         </div>
