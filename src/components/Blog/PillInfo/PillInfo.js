8d978cc8363bf6009954f909f4485703592db173
unknown <zachariah11jerobert@gmail.com>
2022-02-10 08:12:46 +0530

    done changes

--- src/components/Blog/PillInfo/PillInfo.js
+++ src/components/Blog/PillInfo/PillInfo.js
@@ -17,18 +17,26 @@ const PillInfo = (props) => {
       </div>
 
       <div className="pillinfo_container__body">
-        <div>
-          <img src={props.pillImage} style={{ height: "250px" }} />
+        <div className="flex-row">
+          <div>
+            <img src={props.pillImage} style={{ height: "250px" }} />
+          </div>
+          <div className="ml-3">
+            <div style={{ color: props.textColor }}>
+              <span style={{ fontSize: "48px", fontWeight: "700" }}>$1</span>
+              <br/>/
+              Per Dose
+            </div>
+            <h5>
+              Previously <br />
+              {props.subtitle}
+            </h5>
+          </div>
         </div>
-        <div style={{ color: props.textColor }}>
-          <span style={{ fontSize: "48px", fontWeight: "700" }}>$1</span>/ Per
-          Dose
-        </div>
-        <h5>Previously <br/>{props.subtitle}</h5>
         <div>
           <button className="pillinfo_container_button">GET IT NOW!</button>
         </div>
-        <p>{props.children}</p>
+        <p className="desc">{props.children}</p>
       </div>
     </div>
   );
